acf744824122c23e9ab040899e896cfcd40415ae
Дмитрий Поляков <electroni_k@mail.ru>
2024-05-04 12:28:25 +0300

    Метро Финал

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import pandas as pd
 import math
 import time
 
@@ -13,22 +12,27 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 chrome_options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=chrome_options)
 
+#Ввод стартовой страницы для поиска
+start_link = "https://online.metro-cc.ru/category/ovoshchi-i-frukty/ovoshchi"
+
+#Города
 city_list = ['Москва', 'Санкт-Петербург']
+
+
 def main():
     try:
         # Максимизация окна браузера для перехода в полноэкранный режим
         driver.maximize_window()
 
+        df = pd.DataFrame(columns=['ГОРОД', 'ID ПРОДУКТА', 'НАИМЕНОВАНИЕ', 'БРЕНД', 'АКТУАЛЬНАЯ ЦЕНА', 'СТАРАЯ ЦЕНА', 'ССЫЛКА'])
+
         #Цикл для каждого города
         for city in city_list:
             # Открытие страницы
-            driver.get("https://online.metro-cc.ru/category/ovoshchi-i-frukty/ovoshchi?from=under_search&page=1")
-
-            if city == city_list[1]:
-                print('ok')
+            driver.get(start_link)
+            time.sleep(2)
 
             #Нажатие на кнопку с адресом
-            time.sleep(1)
             if city == city_list[0]:
                 address_button = driver.find_element(By.XPATH, "//button[@class='header-address__receive-button offline']")
                 address_button.click()
@@ -39,8 +43,6 @@ def main():
             else:
                 address_button = driver.find_element(By.XPATH,"//button[@class='header-address__receive-button pickup']")
                 address_button.click()
-
-
 
             # Нажатие на ссылку "Изменить"
             edit_link = driver.find_element(By.XPATH, "//span[@class='reset-link active-blue-text']")
@@ -60,13 +62,17 @@ def main():
             select_button = driver.find_element(By.XPATH, "//span[@class='simple-button__text'][text()='Выбрать']")
             select_button.click()
 
-            parser()
+            time.sleep(2)
+            parser(city, df)
+
+        df.to_csv('data.csv', index=False, encoding='utf-8-sig')
 
     except Exception as e:
         print(e)
 
 
-def parser():
+def parser(city, df):
+
     time.sleep(4)
     # Получение исходного кода страницы
     div_user = driver.page_source
@@ -93,22 +99,55 @@ def parser():
 
     products_page = []
     for i in range(2, pages+1):
+        # Получение исходного кода страницы
+        div_user = driver.page_source
+
+        # Создание объекта BeautifulSoup для парсинга HTML
+        soup = BeautifulSoup(div_user, 'html.parser')
+
+        time.sleep(2)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        section_element = soup.find('div', id="products-inner")  # Находим элемент <div>
+        section_element = soup.find('div', id="products-inner").find_all('div', class_="catalog-2-level-product-card")  # Находим элемент <div>
         if section_element:  # Проверяем, что элемент был найден
-            article_elements = section_element.find_all('div')  # Находим все элементы <div> внутри найденного <div>
 
-            for article in article_elements:
+            for el in section_element:
+                try:
+                    element_with_text = el.find('div', class_='product-card__content').find('span', class_='simple-button__text')
+                    if element_with_text.text == 'Сообщить о поступлении':
+                        continue
+                except Exception:
+                    pass
 
-                print(article.get_text())
+                title = el.find('a', class_='product-card-name')['title']  # Получаем атрибут 'title' из тега 'a' внутри элемента 'el'
+                href_link = el.find('a', class_='product-card-name')['href']
+                link_product = f'https://online.metro-cc.ru{href_link}'
+                product_id = el['id']  # Получаем значение атрибута 'id' из элемента 'el'
+                try:
+                    penny = str(el.find('span', class_='product-price__sum').find('span', class_='product-price__sum-penny').text.strip())
+                except Exception:
+                    penny = '.00'
+                price = str(el.find('span', class_='product-price__sum-rubles').text.strip()) + penny + str(el.find('span', class_='product-price__unit').text.strip())  # Получаем текстовое содержимое тега 'span' внутри элемента 'el'
+                price_old = 'Отсутствует'
+                product_brand = 'Без бренда'
+                for brand in substrings:
+                    if brand in str(title.lower()):
+                        product_brand = brand.capitalize()
+                try:
+                    price_old = el.find('div', class_='product-unit-prices__old-wrapper').find('span', class_='product-price__sum-rubles').text.strip()
+                except Exception:
+                    pass
+                print(city, product_id, title.capitalize(), product_brand, price, price_old, link_product)
 
-                if 'нет в наличии' in str(article):
-                    continue
+                df.loc[len(df)] = [city, product_id, title, product_brand, price, price_old, link_product]
+
+
+
+
         driver.get(f"https://online.metro-cc.ru/category/ovoshchi-i-frukty/ovoshchi?from=under_search&page={i}")
         time.sleep(2)
 
 
 if __name__ == "__main__":
     main()
-    time.sleep(500)
+    print('Парсинг завершен')
 
